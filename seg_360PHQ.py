ab31b48f28f4e1c5c1def4a59ca06e878ad65c3a
Yiqian Wu <onethousand1250@gmail.com>
2024-05-19 00:31:16 +0800

    Add files via upload

---
+++ seg_360PHQ.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import argparse
+import numpy as np
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+from src.models.modnet import MODNet
+import glob
+from tqdm import tqdm
+if __name__ == '__main__':
+    # define cmd arguments
+    #parser = argparse.ArgumentParser()
+    # parser.add_argument('--input-path', type=str, help='path of input images')
+    # parser.add_argument('--output-path', type=str, help='path of output images')
+    # parser.add_argument('--ckpt-path', type=str, help='path of pre-trained MODNet')
+    # args = parser.parse_args()
+    #
+    # # check input arguments
+    # if not os.path.exists(args.input_path):
+    #     print('Cannot find input path: {0}'.format(args.input_path))
+    #     exit()
+    # if not os.path.exists(args.output_path):
+    #     print('Cannot find output path: {0}'.format(args.output_path))
+    #     exit()
+    # if not os.path.exists(ckpt_path):
+    #     print('Cannot find ckpt path: {0}'.format(ckpt_path))
+    #     exit()
+
+    ckpt_path = 'F:/full-head-dataset/skeleton_estimation/foreground_matting_v2/MODNet-master/pretrained/modnet_webcam_portrait_matting.ckpt'
+
+    # define hyper-parameters
+    ref_size = 512
+
+    # define image to tensor transform
+    im_transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]
+    )
+
+    # create MODNet and load the pre-trained ckpt
+    modnet = MODNet(backbone_pretrained=False)
+    modnet = nn.DataParallel(modnet)
+
+    if torch.cuda.is_available():
+        modnet = modnet.cuda()
+        weights = torch.load(ckpt_path)
+    else:
+        weights = torch.load(ckpt_path, map_location=torch.device('cpu'))
+    modnet.load_state_dict(weights)
+    modnet.eval()
+
+    # inference images
+
+    im_paths = glob.glob('G:/full-head-dataset/final_new_crop/crop/*/*.png')
+    for im_path in tqdm(im_paths):
+
+        matte_path = im_path.replace('crop', 'matte').replace('final_new_matte', 'final_new_crop')
+        if os.path.exists(matte_path):
+            continue
+        # read image
+        im = Image.open(im_path)
+
+        # unify image channels to 3
+        im = np.asarray(im)
+        if len(im.shape) == 2:
+            im = im[:, :, None]
+        if im.shape[2] == 1:
+            im = np.repeat(im, 3, axis=2)
+        elif im.shape[2] == 4:
+            im = im[:, :, 0:3]
+
+        # convert image to PyTorch tensor
+        im = Image.fromarray(im)
+        im = im_transform(im)
+
+        # add mini-batch dim
+        im = im[None, :, :, :]
+
+        # resize image for input
+        im_b, im_c, im_h, im_w = im.shape
+        if max(im_h, im_w) < ref_size or min(im_h, im_w) > ref_size:
+            if im_w >= im_h:
+                im_rh = ref_size
+                im_rw = int(im_w / im_h * ref_size)
+            elif im_w < im_h:
+                im_rw = ref_size
+                im_rh = int(im_h / im_w * ref_size)
+        else:
+            im_rh = im_h
+            im_rw = im_w
+        
+        im_rw = im_rw - im_rw % 32
+        im_rh = im_rh - im_rh % 32
+        im = F.interpolate(im, size=(im_rh, im_rw), mode='area')
+
+        # inference
+        _, _, matte = modnet(im.cuda() if torch.cuda.is_available() else im, True)
+
+        # resize and save matte
+        matte = F.interpolate(matte, size=(im_h, im_w), mode='area')
+        matte = matte[0][0].data.cpu().numpy()
+
+        # check if directory exists]
+        #print(os.path.dirname(matte_path))
+
+        os.makedirs(os.path.dirname(matte_path), exist_ok=True)
+
+
+        #print('save matte to {0}'.format(matte_path))
+
+        Image.fromarray(((matte * 255).astype('uint8')), mode='L').save(os.path.join(matte_path))
+        #exit()
